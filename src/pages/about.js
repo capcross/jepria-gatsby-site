9693fd486469d75d8f0a7dbc7b44b920bbc0fbcf
AlexanderLapygin <AlexanderLapygin@gmail.com>
2018-08-20 16:30:51 +0300

    refactor: удаление лишнего

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AboutPage = () => (
   <div>
     <h1>О нас</h1>
-    <p>Весёлая команда серьёзных разработчиков</p>
+    <p>Всё очень серьёзно...</p>
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ const IndexPage = () => (
   <div>
     <h1 style={{ color: "#050a08" }}>Всем привет !</h1>
     <p>Добро пожаловать на новый сайт JepRia!</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link> */}
   </div>
 )
 
