b96d02627c4ca95ff1e9b0f6988153a362b81f71
AlexanderLapygin <AlexanderLapygin@gmail.com>
2018-08-24 18:19:42 +0300

    feat: добавление заголовка в раздел "Приложения"

--- src/pages/apps.js
+++ src/pages/apps.js
@@ -3,49 +3,50 @@ import Link from 'gatsby-link'
 
 let items = null;
 
-fetch( 
-	"http://jepria.org/feature-json-jepria-backend/LoginServlet?username=alapygin@yandex.ru&password=jepria2017",
-	{
-		method:'GET',
-		credentials: 'include'
-	})
-	.then((auth) => {
-		console.log("RESPONSE: " + auth);
-		fetch( 
-			"http://jepria.org/feature-json-jepria-backend/v1/features",
-			{
-				method:'GET',
-				credentials: 'include',
-				headers: {
-					'Accept': 'application/json',
-					'Access-Control-Request-Method': 'GET',
-					'Access-Control-Request-Headers': 'Content-Type',
-					'Content-Type': 'application/json'
-				}
-			})
-			.then((response) => {
+fetch(
+  "http://jepria.org/feature-json-jepria-backend/LoginServlet?username=alapygin@yandex.ru&password=jepria2017",
+  {
+    method: 'GET',
+    credentials: 'include'
+  })
+  .then((auth) => {
+    console.log("RESPONSE: " + auth);
+    fetch(
+      "http://jepria.org/feature-json-jepria-backend/v1/features",
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Accept': 'application/json',
+          'Access-Control-Request-Method': 'GET',
+          'Access-Control-Request-Headers': 'Content-Type',
+          'Content-Type': 'application/json'
+        }
+      })
+      .then((response) => {
         console.log("RESPONSE: " + JSON.stringify(response))
-        response.json().then(function(data) {
+        response.json().then(function (data) {
           items = data;
         });
-			});
-	});
+      });
+  });
 
 const AppsPage = () => {
   return (
     <div>
+      <h1>Приложения</h1>
       <h2>JR Feature</h2>
       <div>
         {
-            items && items.map(task => {
-              const name = task.name;
-              return (
-                <Link key={task.id} to={`/product/${task.id}`}>
-                  <div>{name}</div>
-                </Link>
-              )
-            })
-          }      
+          items && items.map(task => {
+            const name = task.name;
+            return (
+              <Link key={task.id} to={`/task/${task.id}`}>
+                <div>{name}</div>
+              </Link>
+            )
+          })
+        }
       </div>
     </div>
   )
