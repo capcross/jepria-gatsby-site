3b9958acbbbd8ecb53ecdbb5fffaf8ffe5082f3a
AlexanderLapygin <AlexanderLapygin@gmail.com>
2018-08-24 16:08:05 +0300

    feat: получение данных из jepria.org

--- src/pages/apps.js
+++ src/pages/apps.js
@@ -1,48 +1,40 @@
 import React from 'react'
 
-// <!DOCTYPE html>
-// <html>
-// <script>
-// fetch( 
-// 	"http://jepria.org/feature-json-jepria-backend/LoginServlet?username=alapygin@yandex.ru&password=jepria2017",
-// 	{
-// 		method:'GET',
-// 		credentials: 'include'
-// 	})
-// 	.then((auth) => {
-// 		console.log("RESPONSE: " + auth);
-// 		fetch( 
-// 			"http://jepria.org/feature-json-jepria-backend/v1/features",
-// 			{
-// 				method:'GET',
-// 				credentials: 'include',
-// 				headers: {
-// 					'Accept': 'application/json',
-// 					'Access-Control-Request-Method': 'GET',
-// 					'Access-Control-Request-Headers': 'Content-Type',
-// 					'Content-Type': 'application/json'
-// 				}
-// 			})
-// 			.then((response) => {
-// 			console.log("RESPONSE: " + JSON.stringify(response))
-// 			response.json().then(function(data) {
-// 				document.getElementById("response").innerHTML = JSON.stringify(data);
-// 			});
-// 			});
-// 	});
-// </script>
-// <body>
+let items = null;
 
-// <h1>This is CORS TEST</h1>
-// <p id="response"></p>
-// </body>
-// </html>
-
+fetch( 
+	"http://jepria.org/feature-json-jepria-backend/LoginServlet?username=alapygin@yandex.ru&password=jepria2017",
+	{
+		method:'GET',
+		credentials: 'include'
+	})
+	.then((auth) => {
+		console.log("RESPONSE: " + auth);
+		fetch( 
+			"http://jepria.org/feature-json-jepria-backend/v1/features",
+			{
+				method:'GET',
+				credentials: 'include',
+				headers: {
+					'Accept': 'application/json',
+					'Access-Control-Request-Method': 'GET',
+					'Access-Control-Request-Headers': 'Content-Type',
+					'Content-Type': 'application/json'
+				}
+			})
+			.then((response) => {
+			console.log("RESPONSE: " + JSON.stringify(response))
+			response.json().then(function(data) {
+				items = JSON.stringify(data);
+			});
+			});
+	});
 
 const AppsPage = () => (
   <div>
     <h1>Приложения</h1>
     <p>В работе...</p>
+    <div>{items}</div>
   </div>
 )
 
