7d16475b3ad38bf31f6e9dd8e271a9803b4bb11d
AlexanderLapygin <AlexanderLapygin@gmail.com>
2018-09-10 13:00:51 +0300

    feat: добавление компонента-обёртки

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled from "styled-components"
 
 import Header from '../components/header'
 import Footer from '../components/footer'
 import Menu from '../components/menu'
 import './index.css'
 
+// Create a Wrapper component that'll render a <article> tag with some styles
+const Wrapper = styled.article`
+//  padding: 4em;
+  background: papayawhip;
+`
+
 const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'JepRia new site' },
-        { name: 'keywords', content: 'jepria, library, react' },
-      ]}
-    />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <Menu/>
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+  <Wrapper>
+    <div>
+      <Helmet
+        title={data.site.siteMetadata.title}
+        meta={[
+          { name: 'description', content: 'JepRia new site' },
+          { name: 'keywords', content: 'jepria, library, react' },
+        ]}
+      />
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <Menu/>
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0,
+        }}
+      >
+        {children()}
+      </div>
+      {/* <Footer/> */}
     </div>
-    {/* <Footer/> */}
-  </div>
+  </Wrapper>    
 )
 
 Layout.propTypes = {
